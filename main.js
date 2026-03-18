9b148fa81b45e3e9d5b7366b1ac22b3c01e9aff6
SAMUELNNAJI <9jaflavzmedia@gmail.com>
2026-03-18 10:05:09 +0100

    added window scrollY in the js file

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ navMenu = document.querySelector(".top-nav-container");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle('active')
 });
+
+// Sticky Navigation Logic
+const navBar = document.querySelector(".top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 60) {
+        navBar.classList.add("scrolled");
+    } else {
+        navBar.classList.remove("scrolled");
+    }
+});
